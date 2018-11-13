538f5fa3853bc84e6ca9edfa5d0167268ca6d98c
Julia Ebert <julia@juliaebert.com>
2018-11-13 11:11:04 -0500

    Including useful files

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
-#!/usr/bin/env bash
-
-rm -f bin/simulation
-mkdir -p bin
 cd src
 
-g++ robot.cpp main.cpp shapes.cpp vars.cpp -O3 -lGL -lGLU -lglut -o ../bin/simulation -std=c++11 -fopenmp
+g++ logger.cpp test.cpp KiloSim.cpp robot.cpp kilobot.cpp viewer.cpp -I /usr/include/hdf5/serial/ -L /usr/lib/x86_64-linux-gnu/hdf5/serial/ -lhdf5 -lhdf5_hl_cpp -lhdf5_cpp -lGL -lGLU -lglut -lsfml-graphics -lsfml-window -lsfml-system -o ../bin/kilosim -std=c++11
+
 cd ..
