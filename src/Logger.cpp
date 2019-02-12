20086337ba67c5ee4748cb12e74a0d267132dfef
Richard Barnes <rbarnes@umn.edu>
2019-02-11 22:33:50 -0800

    Minor changes to resolve and suppress compilation warnings in Logger.cpp

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,5 +1,4 @@
 /*
-    KiloSim
 
     Created 2018-10 by Julia Ebert
 */
@@ -10,8 +9,8 @@ namespace KiloSim
 {
 
 Logger::Logger(World *world, std::string file_id, int trial_num, bool overwrite_trials)
-    : m_file_id(file_id),
-      m_world(world),
+    : m_world(world),
+      m_file_id(file_id),
       m_overwrite_trials(overwrite_trials)
 {
     // Create the HDF5 file if it doesn't already exist
@@ -33,6 +32,7 @@ void Logger::set_trial(uint trial_num)
     try
     {
         H5::Group *trialGroup = new H5::Group(m_h5_file->openGroup(m_trial_group_name.c_str()));
+        (void)trialGroup; //Suppresses warning abut trialGroup variable not being used
         if (m_overwrite_trials)
         {
             m_h5_file->unlink(m_trial_group_name.c_str());
@@ -49,6 +49,8 @@ void Logger::set_trial(uint trial_num)
     {
     }
     H5::Group *trialGroup = new H5::Group(m_h5_file->createGroup(m_trial_group_name.c_str()));
+    (void)trialGroup; //Suppresses warning abut trialGroup variable not being used
+
 
     // Create the params group if it doesn't already exist
     m_params_group = create_or_open_group(m_h5_file, m_params_group_name);
@@ -222,6 +224,7 @@ Logger::H5GroupPtr Logger::create_or_open_group(H5FilePtr file, std::string &gro
     {
         group = new H5::Group(file->createGroup(group_name));
     }
+    (void)group; //Suppresses warning about group variable not being used
 }
 
 } // namespace KiloSim
