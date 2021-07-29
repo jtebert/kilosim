f26075d2b2f17f674105a1781d643c2b16c9a43a
Julia Ebert <julia@juliaebert.com>
2021-07-29 17:02:32 -0400

    trying to fix for RTD... again

--- docs/conf.py
+++ docs/conf.py
@@ -38,10 +38,10 @@ breathe_projects = {}
 
 if read_the_docs_build:
     input_dir = '../api/kilosim'
-    output_dir = 'doxygen'
+    output_dir = ''
     configureDoxyfile(input_dir, output_dir)
     subprocess.call('doxygen', shell=True)
-    breathe_projects['Kilosim'] = output_dir + '/xml'
+    breathe_projects['Kilosim'] = output_dir + 'doxygen/xml'
 
 
 # -- Project information -----------------------------------------------------
